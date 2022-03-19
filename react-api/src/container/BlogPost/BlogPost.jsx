b211a45be1e3c26ebfa075fa2d381113c53810ea
Irsyadha Alfyrdhousi <irsyadhaalfyrdhousi@gmail.com>
2022-03-19 22:30:09 +0700

    Pertemuan4

---
+++ react-api/src/container/BlogPost/BlogPost.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import './BlogPost.css';
+import Post from "../../component/BlogPost/Post";
+
+class BlogPost extends Component{
+    state = {
+        listArtikel:[]
+    }
+  
+    componentDidMount(){
+        fetch('http://localhost:3001/posts')
+        .then(response => response.json())
+        .then(jsonHasilAmbilDariApi => {
+            this.setState({
+                listArtikel: jsonHasilAmbilDariApi
+            })
+        })
+    }
+
+    render(){
+        return(
+            <div className="post-artikel">
+                <h2>Daftar Artikel</h2>
+                {
+                    this.state.listArtikel.map(artikel => {
+                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body}/>
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default BlogPost;
